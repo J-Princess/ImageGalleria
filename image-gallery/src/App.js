5cecda4177e8d8bf72ae4df4c67ddfa359bfed97
Oyibo Ufuoma <princessoyibo@gmail.com>
2023-09-21 19:01:25 +0100

    gkh

--- image-gallery/src/App.js
+++ image-gallery/src/App.js
@@ -1,24 +1,44 @@
-import logo from './logo.svg';
+import React, { Suspense } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { ErrorBoundary } from 'react-error-boundary';
 import './App.css';
+import './index.css';
+import Home from './components/Home';
+import UserDef from './components/Realapp';
+import LoginPage from './components/Signin';
+import { Routes, Route } from 'react-router-dom';
+
+
+
+const ErrorFallback = ({ error, resetErrorBoundary }) => {
+  return (
+    <div className="container m-5 p-5">
+      <h2 >An Error Has Occurred somewhere in the App</h2>
+      <p>{error.message}</p>
+      <button onClick={() => resetErrorBoundary}>Go Home</button>
+    </div>
+  );
+};
 
 function App() {
+  let navigate = useNavigate();
+
+  function handleClick() {
+    navigate('/');
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ErrorBoundary FallbackComponent={ErrorFallback} onReset={handleClick}>
+      <div className="App">
+        <Suspense fallback={<div>Loading</div>}>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/dashboard" element={<UserDef/>} />
+          </Routes>
+        </Suspense>
+      </div>
+    </ErrorBoundary>
   );
 }
 
